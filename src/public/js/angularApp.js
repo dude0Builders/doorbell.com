b0bfbc3ce4e02780a42311e81d5854f2e2119537
Rajesh Sharma <rajesh_dude0@live.com>
2017-11-18 21:23:43 +0530

    added public resources

---
+++ src/public/js/angularApp.js
@@ -0,0 +1,81 @@
+var app = angular.module('doorbell', []);
+
+
+// app.directive('autoLocation', function(){
+//   return {
+//     link : function($scope, element, attrs){
+//       autocomplete = new google.maps.places.Autocomplete(
+//         /** @type {HTMLInputElement} */
+//         element, {
+//             types: ['geocode']
+//         });
+
+//         google.maps.event.addDomListener(element, 'focus', function (){
+
+//                   if (navigator.geolocation) {
+//                       navigator.geolocation.getCurrentPosition(function (position) {
+
+//                           var geolocation = new google.maps.LatLng(
+//                           position.coords.latitude, position.coords.longitude);
+//                           var circle = new google.maps.Circle({
+//                               center: geolocation,
+//                               radius: position.coords.accuracy
+//                           });
+//                           autocomplete.setBounds(circle.getBounds());
+
+//                           // Log autocomplete bounds here
+//                           console.log(autocomplete.getBounds());
+//                       });
+//                   }
+//               });
+//     }
+
+
+//   }
+// })
+app.controller('locationController', ['$scope', function ($scope) {
+  $scope.currentLocation = '';
+  //$scope.currentLocation = "Damak -11 , Jhapa, Nepal";
+
+
+  $scope.detectCurrentLocation = function () {
+    console.log("Detecting current Location");
+    if (navigator.geolocation) {
+      return navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      alert("GeoLocation is not supported by the browser");
+      return;
+    }
+    function showPosition(position) {
+      location.latitude = position.coords.latitude;
+      location.longitude = position.coords.longitude;
+
+      var geocoder = new google.maps.Geocoder();
+      var latLng = new google.maps.LatLng(location.latitude, location.longitude);
+      $("#location").geocomplete({
+        bounds:true,
+        LatLngBounds: latLng
+      });
+
+      if (geocoder) {
+        geocoder.geocode({
+          'latLng': latLng
+        }, function (results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            console.log(results[0].formatted_address);
+            $scope.currentLocation =  results[0].formatted_address;
+
+          } else {
+            console.log("Geocoding failed: " + status);
+            $scope.currentLocation = '';
+          }
+        }); //geocoder.geocode()
+      }
+
+    }
+
+  }
+  $scope.detectCurrentLocation();
+
+}]);
+
